f3da3c4ca3d890d15bae96815e43c5b52711f535
Prayag Jain <prayagjain2@gmail.com>
2021-02-27 20:20:29 +0530

    Completed the project! yay!

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Print intro
+echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ _   ___  __
+| | | \ \/ /      Prayag Jain | Hax Guru
+| |_| |\  /       YouTube: https://youtube.com/c/haxguru
+|  _  |/  \       GitHub: https://github.com/Prayag2
+|_| |_/_/\_\      Email: prayagjain2@gmail.com
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+echo 'Installing consave...'
+
+# Copy 'consave' to ~/.local/bin/
+cp ./consave ~/.local/bin
+sudo chmod +x ~/.local/bin/consave
+
+# Done
+echo 'Installed successfully! You can now delete this folder.'
+echo "Try 'consave -h' for more info!"
