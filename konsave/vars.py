a43d05feafb9183796e8e3802349e213e0b95542
Prayag Jain <prayagjain2@gmail.com>
2021-03-06 10:35:41 +0530

    Important bug fixes

--- konsave/vars.py
+++ konsave/vars.py
@@ -11,9 +11,6 @@ CONFIG_FILE = os.path.join(KONSAVE_DIR, "conf.yaml")
 
 export_extension = '.knsv'
 
-if not os.path.exists(KONSAVE_DIR):
-    os.makedirs(KONSAVE_DIR)
-
 if not os.path.exists(PROFILES_DIR):
     os.makedirs(PROFILES_DIR)
 
