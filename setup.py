fb1d0175ce627c927c2cb2f34e5115eefd103c29
Maja Bojarska <majabojarska98@gmail.com>
2021-03-13 17:59:46 +0100

    Reformat setup.py with Black.

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ def read_desc() -> str:
     Returns:
         str: Contents of README.md file.
     """
-    with open('README.md', 'r') as desc:
+    with open("README.md", "r") as desc:
         return desc.read()
 
 
 setup(
     name="Konsave",
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     author="Prayag Jain",
     author_email="prayagjain2@gmail.com",
     description="A program that lets you save your Plasma configuration in an instant!",
@@ -25,20 +25,16 @@ setup(
     long_description_content_type="text/markdown",
     url="https://www.github.com/prayag2/konsave/",
     packages=find_packages(),
-    package_data={'config': ['conf.yaml']},
+    package_data={"config": ["conf.yaml"]},
     include_package_data=True,
-    python_requires='>=3.6',
-    install_requires=['PyYaml'],
+    python_requires=">=3.6",
+    install_requires=["PyYaml"],
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: POSIX",
         "Environment :: Console",
         "Intended Audience :: End Users/Desktop",
-        'Programming Language :: Python'
+        "Programming Language :: Python",
     ],
-    entry_points={
-        'console_scripts': [
-            "konsave = konsave.__main__:main"
-        ]
-    }
+    entry_points={"console_scripts": ["konsave = konsave.__main__:main"]},
 )
