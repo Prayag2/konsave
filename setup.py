90c887ec6df2f65c764ab0bc7922cea1d389c13f
Maja Bojarska <majabojarska98@gmail.com>
2021-03-13 18:11:30 +0100

    Refactor setup.py to use separate pip requirement files.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-"""
-Setup module
-"""
+"""Setup module"""
+
+from pathlib import Path
+from typing import List
+
 from setuptools import setup, find_packages
 
 
-def read_desc() -> str:
-    """Reads the README.
+def _read_desc() -> str:
+    """Reads the README.md.
 
     Returns:
         str: Contents of README.md file.
@@ -14,6 +16,23 @@ def read_desc() -> str:
         return desc.read()
 
 
+def _read_reqs(path: Path) -> List[str]:
+    """Reads a pip requirement file.
+
+    Args:
+        path (Path): Path to pip requirements file.
+
+    Returns:
+        :type: list of str: List of dependency identifiers.
+    """
+    with open(path, "r") as file:
+        return file.readlines()
+
+
+# Package requirements
+_REQUIREMENTS: List[str] = _read_reqs(Path("requirements.txt"))
+_REQUIREMENTS_DEV: List[str] = _read_reqs(Path("requirements_dev.txt"))
+
 setup(
     name="Konsave",
     use_scm_version=True,
@@ -21,14 +40,15 @@ setup(
     author="Prayag Jain",
     author_email="prayagjain2@gmail.com",
     description="A program that lets you save your Plasma configuration in an instant!",
-    long_description=read_desc(),
+    long_description=_read_desc(),
     long_description_content_type="text/markdown",
     url="https://www.github.com/prayag2/konsave/",
     packages=find_packages(),
     package_data={"config": ["conf.yaml"]},
     include_package_data=True,
     python_requires=">=3.6",
-    install_requires=["PyYaml"],
+    install_requires=_REQUIREMENTS,
+    extras_require={"dev": _REQUIREMENTS_DEV},
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: POSIX",
