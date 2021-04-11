c5084fbdcf35c8a5ce15c3e34dd890a4bacb8a57
Prayag Jain <prayagjain2@gmail.com>
2021-04-11 14:31:47 +0530

    Changed `vars.py` to `consts.py`

---
+++ konsave/consts.py
@@ -0,0 +1,23 @@
+"""
+This module contains all the variables for konsave
+"""
+import os
+
+
+HOME = os.path.expandvars("$HOME")
+CONFIG_DIR = os.path.join(HOME, ".config")
+KONSAVE_DIR = os.path.join(CONFIG_DIR, "konsave")
+PROFILES_DIR = os.path.join(KONSAVE_DIR, "profiles")
+CONFIG_FILE = os.path.join(KONSAVE_DIR, "conf.yaml")
+
+EXPORT_EXTENSION = ".knsv"
+
+# Create PROFILES_DIR if it doesn't exist
+if not os.path.exists(PROFILES_DIR):
+    os.makedirs(PROFILES_DIR)
+
+list_of_profiles = os.listdir(PROFILES_DIR)
+length_of_lop = len(list_of_profiles)
+
+# Current Version
+VERSION = "2.0.0-alpha.1"
